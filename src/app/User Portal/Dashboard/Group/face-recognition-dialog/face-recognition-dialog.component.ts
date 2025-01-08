b3d865e2e0c4164fd35e7889c42ce1df5b8f5fda
zhenee0615 <zhenee0615@yahoo.com>
2025-01-08 22:54:40 +0800

    .

--- src/app/User Portal/Dashboard/Group/face-recognition-dialog/face-recognition-dialog.component.ts
+++ src/app/User Portal/Dashboard/Group/face-recognition-dialog/face-recognition-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnDestroy, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
 
